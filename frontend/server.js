d94c304615a623e4528392bcb980ac0bfc390527
kujira <test5151@protonmail.com>
2025-04-03 11:47:11 +0900

    feat: トップページのレイアウトを更新 - aboutページの内容を統合し、新着人気スレッドを追加

--- frontend/server.js
+++ frontend/server.js
@@ -6,9 +6,28 @@ const port = 8080;
 // 静的ファイルの提供
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ルートパスを管理者ダッシュボードにリダイレクト
+// CSPヘッダーの設定
+app.use((req, res, next) => {
+    res.setHeader(
+        'Content-Security-Policy',
+        "default-src 'self'; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
+        "font-src 'self' https://fonts.gstatic.com; " +
+        "img-src 'self' data: https:; " +
+        "connect-src 'self' http://localhost:3000;"
+    );
+    next();
+});
+
+// ルートパスをindex.htmlにルーティング
 app.get('/', (req, res) => {
-    res.redirect('/admin');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// aboutページのルート
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(port, () => {
